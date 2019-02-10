db8a1890e780e3d08690caf227f09c7a834a48a8
Veldin <amr.jonkman@gmail.com>
2019-02-10 22:16:38 +0100

    Added basic timekeeping and keyboard reading capabilities. Also setup the Canvas

--- AdvancedWarsEngine/MainWindow.xaml.cs
+++ AdvancedWarsEngine/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,102 @@ namespace AdvancedWarsEngine
     /// </summary>
     public partial class MainWindow : Window
     {
+        //For keeping the sizes of the window (on the canvas, not IRL)
+        private int width;
+        private int height;
+
+        //For timekeeping (we need to know when the last frame happend when the next frame happens and the delta between)
+        private long delta;     //The lenght in time the last frame lasted (so we can use it to calculate speeds of things without slowing down due to low fps)
+        private long now;       //This is the time of the frame. (To calculate the delta)
+        private long then;      //This is the time of the previous frame. (To calculate the delta)
+
+        //The max fps we want to run at
+        private float fps;  //The set FPS limit
+        private float interval; //Interfal that gets calculated based on the fps
+
+        //Holds string interpertation of all keys that are pressed right now
+        private HashSet<String> pressedKeys;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            width = 1280;
+            height = 720;
+            pressedKeys = new HashSet<String>();
+
+            //Bind the keyup/down to the window's keyup/down
+            Window.GetWindow(this).KeyUp += KeyUp;
+            Window.GetWindow(this).KeyDown += KeyDown;
+
+            InitializeComponent();
+
+            fps = 999999999; //Desired max fps.
+            interval = 1000 / fps;
+            then = Stopwatch.GetTimestamp();
+
+            Run();
+        }
+
+        public void Run()
+        {
+
+            now = Stopwatch.GetTimestamp();
+            delta = (now - then) / 1000; //Defide by 1000 to get the delta in MS
+
+            if (delta > interval)
+            {
+                then = now; //Remember when this frame was.
+                //Debug.WriteLine(delta);
+
+                //Logic(delta); //Run the logic of the simulation.
+                //Draw();
+            }
+            else
+            {
+                /*
+                 * Sleeping the thread for the minimum amount of time. This helps with the stability of the application.
+                 * While not hindering the user experience.
+                 * */
+                Thread.Sleep(1);
+            }
+
+            Task.Yield();  //Force this task to complete asynchronously (This way the main thread is not blocked by this task calling itself.
+            Task.Run(() => Run());  //Schedule new Run() task
+        }
+
+        /* KeyDown */
+        /* 
+        * Add the given key in the pressedKeys collection.
+        * The argument is the given key represented as a string.
+        */
+        public void KeyDown(object sender, KeyEventArgs args)
+        {
+            //pressedKeys.Add(args.Key.ToString());
+            Debug.WriteLine(args.Key.ToString());
+        }
+
+
+        /* KeyDown */
+        /* 
+         * Remove the given key in the pressedKeys collection.
+         * The argument is the given key represented as a string.
+         */
+        public void KeyUp(object sender, KeyEventArgs args)
+        {
+            //pressedKeys.Remove(args.Key.ToString());
+            Debug.WriteLine(args.Key.ToString());
+        }
+
+        /* IsKeyPressed */
+        /* 
+         * Returns wheater the given key exists within the pressedKeys collection.
+         * The argument is the given key represented as a string.
+         */
+        public Boolean IsKeyPressed(String virtualKey)
+        {
+            //return pressedKeys.Contains(virtualKey);
+            return false;
         }
     }
 }
