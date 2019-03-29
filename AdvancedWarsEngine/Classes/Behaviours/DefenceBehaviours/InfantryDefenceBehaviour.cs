ffcf5ff85ea748450be85536d57bb29bbab1596b
Maryn <marynkaspers20@gmail.com>
2019-03-29 14:28:28 +0100

    semantics

--- AdvancedWarsEngine/Classes/Behaviours/DefenceBehaviours/InfantryDefenceBehaviour.cs
+++ AdvancedWarsEngine/Classes/Behaviours/DefenceBehaviours/InfantryDefenceBehaviour.cs
@@ -5,11 +5,11 @@
         public float Defence(GameObject unit, Tile tile)
         {
             // Setting some local variables
-            float baseValue = 5;                       // The baseValue
-            float defenceValue = baseValue;                // The total defence value which will be returned
+            float baseValue = 5;                            // The baseValue
+            float defenceValue = baseValue;                 // The total defence value which will be returned
 
             // Multiply the defenceValue based on the type of tile it's standing on
-            switch (tile.GetType().Name)     //Todo make enums or something for this
+            switch (tile.GetType().Name)                    //Todo make enums or something for this
             {
                 case "Mountain":
                     defenceValue += baseValue * 2;
@@ -22,7 +22,7 @@
                 case "Urban":
                     defenceValue += baseValue * 2;
                     break;
-                case "Water":                           // Infantry cannot stand on Water
+                case "Water":                               // Infantry cannot stand on Water
                 case "Road":
                     break;
             }
--- AdvancedWarsEngine/Classes/Behaviours/DefenceBehaviours/VehicleDefenceBehaviour.cs
+++ AdvancedWarsEngine/Classes/Behaviours/DefenceBehaviours/VehicleDefenceBehaviour.cs
@@ -5,8 +5,8 @@
         public float Defence(GameObject unit, Tile tile)
         {
             // Setting some local variables
-            float baseValue = 5;                       // The baseValue
-            float defenceValue = baseValue;                // The total defence value which will be returned
+            float baseValue = 5;                            // The baseValue
+            float defenceValue = baseValue;                 // The total defence value which will be returned
 
             // Multiply the defenceValue based on the type of tile it's standing on
             switch (tile.GetType().Name)                    //Todo make enums or something for this
